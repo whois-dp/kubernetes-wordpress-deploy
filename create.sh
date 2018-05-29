5410e3a22cfd4d74afec27e62475361056f9b7fe
dp <poge@mailbox.org>
2018-05-29 17:19:20 +0200

    Update create.sh
    delete stuff

--- create.sh
+++ create.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-kubectl create configmap varnish-vcl --from-file=configs/default.vcl
-kubectl create -f varnish.yaml
 kubectl create -f mariadb.yaml
 kubectl create -f wordpress.yaml
 kubectl create -f ingress.yaml
